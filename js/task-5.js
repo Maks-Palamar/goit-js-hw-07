de02e1aa2eed5517366d748f83c887cbd3563950
Maksym Palamarchuk <97300407+Maks-Palamar@users.noreply.github.com>
2024-01-08 21:51:50 +0200

    tsk5 upd

--- js/task-5.js
+++ js/task-5.js
@@ -1,55 +1,42 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
 
 const toColor = document.querySelector('body');
 const toSpan = document.querySelector('.color');
 const toBtn = document.querySelector('.change-color');
-const btnListener = toBtn.addEventListener("click", changeColBtn);
-
 let colr;
-let isChangeColBtnCalled = false;
 
 function changeColBtn(event) {
   let currentCol = getRandomHexColor();
   toColor.style.backgroundColor = currentCol;
-  toColor.style.transition = 'background-color 0.5s';
+  toColor.style.transition = 'background-color 0.6s';
   toSpan.textContent = currentCol;
-  toSpan.style.transition = 'color 0.5s'
+  toSpan.style.transition = 'color 0.6s';
   toBtn.style.backgroundColor = currentCol;
   toBtn.style.borderStyle = 'solid';
   toBtn.style.borderColor = currentCol;
   toBtn.style.color = '#fff';
-  toBtn.style.transition = 'background-color 0.5s, border-color 0.5s, color 0.5s';
+  toBtn.style.transition = 'background-color 0.6s, border-color 0.6s, color 0.6s';
 
   colr = currentCol;
-
-  isChangeColBtnCalled = true;
+  toBtn.addEventListener('mouseover', newOnStyle);
+  toBtn.addEventListener('mouseout', newOutStyle);
+  
 }
 
-if (isChangeColBtnCalled) {
-  toBtn.addEventListener('mouseover', function() {
-    toBtn.style.borderStyle = 'solid';
-    toBtn.style.borderColor = colr;
-    toBtn.style.backgroundColor = '#fff';
-    toBtn.style.color = colr;
-  });
-  
-  toBtn.addEventListener('mouseout', function() {
-    toBtn.style.backgroundColor = colr;
-    toBtn.style.color = '#fff';
-  });
-}else {
-  toBtn.addEventListener('mouseover', function() {
-    toBtn.style.borderStyle = 'solid';
-    toBtn.style.borderColor = '#4E75FF';
-    toBtn.style.color = '#4E75FF';
-  });
-  
-  toBtn.addEventListener('mouseout', function() {
-    toBtn.style.backgroundColor = '#4E75FF';
-    toBtn.style.color = '#fff';
-  });
+function newOnStyle() {
+  toBtn.style.borderStyle = 'solid';
+  toBtn.style.borderColor = colr;
+  toBtn.style.backgroundColor = '#fff';
+  toBtn.style.color = colr;
 }
+
+function newOutStyle() {
+  toBtn.style.backgroundColor = colr;
+  toBtn.style.color = '#fff';
+}
+
+toBtn.addEventListener('click', changeColBtn);
