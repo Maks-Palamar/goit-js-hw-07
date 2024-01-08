adc9ee1d12a25a4486bdacbd5ad2215b6c4d59e5
Maksym Palamarchuk <97300407+Maks-Palamar@users.noreply.github.com>
2024-01-08 20:33:00 +0200

    tsk 4&5 temp

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector('.login-form');
+form.addEventListener("submit", formSubmit);
 
+function formSubmit(event) {
+    event.preventDefault();
+    const formTarg = event.target;
+    const eMail = formTarg.elements.email.value;
+    const passWrd = formTarg.elements.password.value;
+
+    if (eMail === "" || passWrd === "") {
+        return console.log("Please fill in all the fields!");
+      }
+
+    const uData = {
+        email: eMail,
+        password: passWrd,
+    }
+
+    console.log(uData);
+
+    form.reset();
+}
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,53 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const toColor = document.querySelector('body');
+const toSpan = document.querySelector('.color');
+const toBtn = document.querySelector('.change-color');
+const btnListener = toBtn.addEventListener("click", changeColBtn);
+
+let colr;
+let isChangeColBtnCalled = false;
+
+function changeColBtn(event) {
+  let currentCol = getRandomHexColor();
+  toColor.style.backgroundColor = currentCol;
+  toColor.style.transition = 'background-color 0.5s';
+  toSpan.textContent = currentCol;
+  toSpan.style.transition = 'color 0.5s'
+  toBtn.style.backgroundColor = currentCol;
+  toBtn.style.borderStyle = 'solid';
+  toBtn.style.borderColor = currentCol;
+  toBtn.style.color = '#fff';
+  toBtn.style.transition = 'background-color 0.5s, border-color 0.5s, color 0.5s';
+
+  colr = currentCol;
+
+  isChangeColBtnCalled = true;
+}
+
+if (isChangeColBtnCalled) {
+  toBtn.addEventListener('mouseover', function() {
+    toBtn.style.borderStyle = 'solid';
+    toBtn.style.borderColor = colr;
+    toBtn.style.backgroundColor = '#fff';
+    toBtn.style.color = colr;
+  });
+  
+  toBtn.addEventListener('mouseout', function() {
+    toBtn.style.backgroundColor = colr;
+    toBtn.style.color = '#fff';
+  });
+}else {
+  toBtn.addEventListener('mouseover', function() {
+    toBtn.style.borderStyle = 'solid';
+    toBtn.style.borderColor = '#4E75FF';
+    toBtn.style.color = '#4E75FF';
+  });
+  
+  toBtn.addEventListener('mouseout', function() {
+    toBtn.style.backgroundColor = '#4E75FF';
+    toBtn.style.color = '#fff';
+  });
+}
