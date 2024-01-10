160ff37e2aab565113b7b72123687252fbb8ca51
Maksym Palamarchuk <97300407+Maks-Palamar@users.noreply.github.com>
2024-01-10 02:25:55 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,9 @@
 const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
+const defaultName = nameOutput.textContent;
+
 nameInput.addEventListener("input", (event) => {
-  nameOutput.textContent = event.currentTarget.value;
+  const outputValue = event.currentTarget.value.trim() || defaultName;
+  nameOutput.textContent = outputValue;
 });
