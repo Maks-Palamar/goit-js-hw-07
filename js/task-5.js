6e9b9d1bfb6b8039f58143f1befb6980d8906152
Maksym Palamarchuk <97300407+Maks-Palamar@users.noreply.github.com>
2024-01-16 03:44:53 +0200

    fix 2 additional functional removal

--- js/task-5.js
+++ js/task-5.js
@@ -15,28 +15,6 @@ function changeColBtn(event) {
   toColor.style.transition = 'background-color 0.6s';
   toSpan.textContent = currentCol;
   toSpan.style.transition = 'color 0.6s';
-  /*toBtn.style.backgroundColor = currentCol;
-  toBtn.style.borderStyle = 'solid';
-  toBtn.style.borderColor = currentCol;
-  toBtn.style.color = '#fff';
-  toBtn.style.transition = 'background-color 0.6s, border-color 0.6s, color 0.6s';
-  colr = currentCol;
-  */
 }
 
-/*toBtn.addEventListener('mouseover', newOnStyle);
-toBtn.addEventListener('mouseout', newOutStyle);
-
-function newOnStyle() {
-  toBtn.style.borderStyle = 'solid';
-  toBtn.style.borderColor = colr;
-  toBtn.style.backgroundColor = '#fff';
-  toBtn.style.color = colr;
-}
-
-function newOutStyle() {
-  toBtn.style.backgroundColor = colr;
-  toBtn.style.color = '#fff';
-}
-*/
 toBtn.addEventListener('click', changeColBtn);
